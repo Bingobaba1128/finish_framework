b05c407c17c577cdca5c6d1300ea19d3520269d2
赵鑫淼 <zhaoxinmiao@zhaoxiniaodeMBP.mshome.net>
2020-05-21 09:49:11 +0800

    first upload

---
+++ src/store/store.js
@@ -0,0 +1,50 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store ({
+
+  state: {
+    user: '',
+    company: '',
+    showCompany:false
+  },
+
+  getters: {
+    
+  },
+
+  mutations: {
+    addUserInfo(state,info) {
+      state.user = info
+    },
+    getCompany(state){
+      state.user.authorities.map( (item,index) => {
+        if(item.parentId == '0'){
+          window.console.log(item.displayName,index)
+        }
+      })      
+    },
+    showComp(state){
+      state.showCompany = true
+    } 
+  },
+
+  actions: {
+    addUser (context,info){
+      context.commit("addUserInfo",info)
+    },
+    getCompanyList(context){
+      context.commit("getCompany")
+    },
+    showCompanyName(context) {
+      context.commit("showComp")
+      window.console.log('ooppppp')
+    },
+  }
+
+
+})
+
+export default store
